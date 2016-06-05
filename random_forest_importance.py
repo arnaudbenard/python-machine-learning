3b8ab5fe795245ca563b376289f4fb50c0ed14ca
Arnaud Benard <arnaudm.benard@gmail.com>
2016-06-05 12:19:10 +0100

    use random forest for feature importance

---
+++ random_forest_importance.py
@@ -0,0 +1,36 @@
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+from sklearn.base import clone
+from itertools import combinations
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+
+df_wine = pd.read_csv('https://archive.ics.uci.edu/'
+                      'ml/machine-learning-databases/wine/wine.data',
+                      header=None)
+
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total phenols',
+                   'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins',
+                   'Color intensity', 'Hue', 'OD280/OD315 of diluted wines',
+                   'Proline']
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+
+X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, test_size=0.3, random_state=0)
+
+feat_labels = df_wine.columns[1:]
+forest = RandomForestClassifier(n_estimators=10000, random_state=0, n_jobs=-1)
+forest.fit(X_train, y_train)
+importances = forest.feature_importances_
+indices = np.argsort(importances)[::-1]
+
+for f in range(X_train.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30,
+                            feat_labels[indices[f]],
+                            importances[indices[f]]))
--- sbs.py
+++ sbs.py
@@ -1,7 +1,7 @@
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import datasets
+import pandas as pd
 from sklearn.base import clone
 from itertools import combinations
 import numpy as np
@@ -11,7 +11,7 @@ from sklearn.metrics import accuracy_score
 # Sequential Backward Selection
 
 
-class SBS(object):
+class SBS():
     def __init__(self, estimator, k_features, scoring=accuracy_score,
                  test_size=0.25, random_state=1):
         self.scoring = scoring
@@ -21,16 +21,16 @@ class SBS(object):
         self.random_state = random_state
 
     def fit(self, X, y):
+
         X_train, X_test, y_train, y_test = \
-            train_test_split(X, y,
-                             test_size=self.test_size,
+            train_test_split(X, y, test_size=self.test_size,
                              random_state=self.random_state)
+
         dim = X_train.shape[1]
         self.indices_ = tuple(range(dim))
         self.subsets_ = [self.indices_]
-        score = self._calc_score(X_train, y_train, X_test, y_test,
-                                 self.indices_)
-
+        score = self._calc_score(X_train, y_train,
+                                 X_test, y_test, self.indices_)
         self.scores_ = [score]
 
         while dim > self.k_features:
@@ -38,8 +38,8 @@ class SBS(object):
             subsets = []
 
             for p in combinations(self.indices_, r=dim - 1):
-                score = self._calc_score(X_train, y_train, X_test, y_test,
-                                         p)
+                score = self._calc_score(X_train, y_train,
+                                         X_test, y_test, p)
                 scores.append(score)
                 subsets.append(p)
 
@@ -49,7 +49,6 @@ class SBS(object):
             dim -= 1
 
             self.scores_.append(scores[best])
-
         self.k_score_ = self.scores_[-1]
 
         return self
@@ -65,12 +64,13 @@ class SBS(object):
 
 
 # Test the SBS
-iris = datasets.load_iris()
-X = iris.data[:, [2, 3]]
-y = iris.target
+df_wine = pd.read_csv('https://archive.ics.uci.edu/'
+                      'ml/machine-learning-databases/wine/wine.data',
+                      header=None)
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, test_size=0.3, random_state=0)
 
 # Feature scaling - computes mean and standard deviation
 sc = StandardScaler()
@@ -82,10 +82,26 @@ knn = KNeighborsClassifier(n_neighbors=2)
 sbs = SBS(knn, k_features=1)
 sbs.fit(X_train_std, y_train)
 
+
 k_feat = [len(k) for k in sbs.subsets_]
-plt.plot(k_feat, sbs.scores_, marker='o')
-plt.ylim([0.7, 1.1])
-plt.ylabel('Accuracy')
-plt.xlabel('Number of features')
-plt.grid()
-plt.show()
+
+# plt.plot(k_feat, sbs.scores_, marker='o')
+# plt.ylim([0.7, 1.1])
+# plt.xlim([0, 20])
+# plt.ylabel('Accuracy')
+# plt.xlabel('Number of features')
+# plt.grid()
+# plt.show()
+
+k5 = list(sbs.subsets_[8])
+print('Selected top 5 features:\n', df_wine.columns[1:][k5])
+
+knn.fit(X_train_std, y_train)
+print('\nPerformance using all features:\n')
+print('Training accuracy:', knn.score(X_train_std, y_train))
+print('Test accuracy:', knn.score(X_test_std, y_test))
+
+knn.fit(X_train_std[:, k5], y_train)
+print('\nPerformance using the top 5 features:\n')
+print('Training accuracy:', knn.score(X_train_std[:, k5], y_train))
+print('Test accuracy:', knn.score(X_test_std[:, k5], y_test))
